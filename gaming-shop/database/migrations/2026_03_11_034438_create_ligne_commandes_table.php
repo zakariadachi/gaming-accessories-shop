6506eba9cc9d0bf98e1cf6e6f8fe2643fe0e494d
zakariadachi <dachiziko@gmail.com>
2026-03-17 02:37:32 +0000

    ajout de la migration pour la table ligne_commandes

---
+++ gaming-shop/database/migrations/2026_03_11_034438_create_ligne_commandes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ligne_commandes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('quantity');
+            $table->foreignId('commande_id')->constrained('commandes')->onDelete('cascade');
+            $table->foreignId('produit_id')->constrained('produits')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ligne_commandes');
+    }
+};
