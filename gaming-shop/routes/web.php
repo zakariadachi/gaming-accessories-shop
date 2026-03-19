df433eeafbafe4ce7d294876e082641b041a10be
zakariadachi <dachiziko@gmail.com>
2026-03-19 05:34:12 +0000

    ajout des routes login, register et logout

--- gaming-shop/routes/web.php
+++ gaming-shop/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('guest')->group(function () {
+    Route::get('/register', fn() => view('auth.register'))->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+
+    Route::get('/login', fn() => view('auth.login'))->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', fn() => view('dashboard'))->name('dashboard');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
