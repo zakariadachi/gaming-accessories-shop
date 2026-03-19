cb35dc7183537aaeec6d8cbfc85fa7a0f7463d10
zakariadachi <dachiziko@gmail.com>
2026-03-19 05:41:49 +0000

    ajout du contrôleur ProduitController avec index et show

---
+++ gaming-shop/app/Http/Controllers/ProduitController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Produit;
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class ProduitController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Produit::with('categorie');
+
+        if ($request->filled('categorie')) {
+            $query->where('categorie_id', $request->categorie);
+        }
+
+        if ($request->filled('search')) {
+            $query->where('nom', 'like', '%' . $request->search . '%');
+        }
+
+        $produits   = $query->paginate(12);
+        $categories = Categorie::all();
+
+        return view('produits.index', compact('produits', 'categories'));
+    }
+
+    public function show(Produit $produit)
+    {
+        $produit->load('categorie');
+
+        $similaires = Produit::where('categorie_id', $produit->categorie_id)
+            ->where('id', '!=', $produit->id)
+            ->limit(4)
+            ->get();
+
+        return view('produits.show', compact('produit', 'similaires'));
+    }
+}
