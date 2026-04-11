d66b71f2ed51464082edf00d06bd6886a2d0a154
zakariadachi <dachiziko@gmail.com>
2026-04-11 08:51:38 +0100

    mise à jour des routes : communauté publique et ajout commandes

--- gaming-shop/routes/web.php
+++ gaming-shop/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\CartController;
+use App\Http\Controllers\CommandeController;
 use App\Http\Controllers\ProduitController;
 use App\Http\Controllers\ProfilController;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,7 @@ Route::get('/home', [\App\Http\Controllers\HomeController::class, 'index'])->nam
 
 Route::get('/produits', [ProduitController::class, 'index'])->name('produits.index');
 Route::get('/produits/{produit}', [ProduitController::class, 'show'])->name('produits.show');
+Route::get('/communaute', fn() => view('communaute.index'))->name('communaute.index');
 
 Route::middleware('guest')->group(function () {
     Route::get('/register', fn() => view('auth.register'))->name('register');
@@ -32,9 +34,13 @@ Route::middleware('auth')->group(function () {
     Route::put('/profil/infos', [ProfilController::class, 'updateInfos'])->name('profil.infos');
     Route::put('/profil/password', [ProfilController::class, 'updatePassword'])->name('profil.password');
 
-    Route::get('/cart', fn() => redirect()->route('produits.index'))->name('cart.index');
+    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
     Route::post('/cart/{produit}', [CartController::class, 'add'])->name('cart.add');
     Route::put('/cart/{id}', [CartController::class, 'update'])->name('cart.update');
     Route::delete('/cart/{id}', [CartController::class, 'remove'])->name('cart.remove');
     Route::delete('/cart', [CartController::class, 'clear'])->name('cart.clear');
+
+    Route::post('/commandes', [CommandeController::class, 'store'])->name('commandes.store');
+    Route::get('/commandes', [CommandeController::class, 'index'])->name('commandes.index');
+    Route::get('/commandes/{commande}', [CommandeController::class, 'show'])->name('commandes.show');
 });
