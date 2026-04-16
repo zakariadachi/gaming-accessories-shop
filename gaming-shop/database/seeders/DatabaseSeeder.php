b25823e8d5564cae4146d061311c2ce0df615514
zakariadachi <dachiziko@gmail.com>
2026-04-16 09:26:29 +0100

    feat: mise à jour du DatabaseSeeder

--- gaming-shop/database/seeders/DatabaseSeeder.php
+++ gaming-shop/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorieSeeder::class,
             ProduitSeeder::class,
+            CommandeSeeder::class,
         ]);
     }
 }
