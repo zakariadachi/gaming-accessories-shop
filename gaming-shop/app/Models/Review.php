c2f30763f63534d48ce0e4412561c71da6b89cd9
zakariadachi <dachiziko@gmail.com>
2026-04-22 11:38:54 +0100

    feat: création du modèle Review

---
+++ gaming-shop/app/Models/Review.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'produit_id',
+        'note',
+        'commentaire',
+    ];
+
+    protected $casts = [
+        'note' => 'integer',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function produit(): BelongsTo
+    {
+        return $this->belongsTo(Produit::class);
+    }
+}
